fba6e340ccfed965fed3ebf8cd1183aaecf772b3
suwandre <suwandresukijat@gmail.com>
2022-11-16 12:29:40 +0100

    reimplemented utils files

---
+++ utils/getQuiz.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const Moralis = require('moralis-v1/node');
+const { parseJSON } = require('./jsonParser');
+const getQuiz = async () => {
+    try {
+        await Moralis.start({
+            appId: process.env.NEXT_PUBLIC_MORALIS_APPID,
+            serverUrl: process.env.NEXT_PUBLIC_MORALIS_SERVERURL,
+            masterKey: process.env.NEXT_PUBLIC_MORALIS_MASTERKEY,
+        });
+        const QuizData = new Moralis.Query('RealmHunterQuizBot');
+        const quizData = await QuizData.find({useMasterKey: true});
+
+        if (quizData.length === 0 || !quizData) {
+            throw new Error('No quiz data found');
+        }
+
+        const result = parseJSON(quizData);
+    
+        // an array of question data objects which include the question, answer(s) and other important data
+        const questionDatas = [];
+        result.forEach((questionItem) => {
+            const questionData = {
+                questionId: questionItem.questionId,
+                question: questionItem.question,
+                answers: questionItem.answers,
+                correctAnswers: questionItem.correctAnswers,
+                minimumPoints: questionItem.minimumPoints,
+                maximumPoints: questionItem.maximumPoints,
+                duration: questionItem.duration,
+            }
+    
+            questionDatas.push(questionData);
+        });
+        
+        return questionDatas;
+    } catch (err) {
+        throw err;
+    }
+}
+
+module.exports = {getQuiz};
